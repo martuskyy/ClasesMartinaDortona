451e9f68677d236e2b3631a9de06fd2726b129cc
Windows11 <Windows11@DESKTOP-7UK5LJ2>
2024-01-15 15:00:14 -0300

    Agregar archivos de proyecto.

---
+++ Clases/Producto.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class Producto
+    {
+        private int _id;
+        private string _descripcion;
+        private double _costo;
+        private double _precioVenta;
+        private int _stock;
+        private int _idUsuario;
+
+        public Producto(int id, string descripcion, double costo, double precioVenta, int stock, int idUsuario)
+        {
+            this._id = id;
+            this._descripcion = descripcion;
+            this._costo = costo;
+            this._precioVenta = precioVenta;
+            this._stock = stock;
+            this._idUsuario = idUsuario;
+        }
+
+        public int Id { get { return this._id; } set { this._id = value; } }
+        public string Descripcion { get { return this._descripcion; } set { this._descripcion = value; } }
+        public double Costo { get { return this._costo; } set { this._costo = value; } }
+        public double PrecioVenta { get { return this._precioVenta; } set { this._precioVenta = value; } }
+        public int Stock { get { return this._stock; } set { this._stock = value; } }
+        public int IdUsuario { get { return this._idUsuario; } set { this._idUsuario = value; } }
+    }
+}
---
+++ Clases/Venta.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases
+{
+    internal class Venta
+    {
+        private int _id;
+        private string _comentario;
+        private int _idUsuario;
+
+        public Venta(int id, string comentario, int idUsuario)
+        {
+            this._id = id;
+            this._comentario = comentario;
+            this._idUsuario = idUsuario;
+        }
+
+        public int Id { get { return this._id; } set { this._id = value; } }
+        public string Comentario { get { return this._comentario; } set { this._comentario = value; } }
+        public int IdUsuario { get { return this._idUsuario; } set { this._idUsuario = value; } }
+    }
+}
